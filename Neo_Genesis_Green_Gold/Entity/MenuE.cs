3f86aa1d1a88009877492dbc05f91671a69f736a
hmsr9508 <hectormsr951008@gmail.com>
2024-09-23 17:17:01 -0600

    Menu entity

---
+++ Neo_Genesis_Green_Gold/Entity/MenuE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entity
+{
+    public class MenuE
+    {
+        public int ID_Menu { get; set; }
+        public string Nombre_Menu { get; set; }
+        public int? ID_PadreMenu { get; set; }
+        public DateTime Fecha_Inserto { get; set; }
+
+        // Relación con Menu_Usuario
+        public ICollection<Menu_UsuarioE> Menu_Usuarios { get; set; }
+    }
+}
---
+++ Neo_Genesis_Green_Gold/Entity/Menu_UsuarioE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Entity
+{
+    public class Menu_UsuarioE
+    {
+        public int ID_Menu_Usuario { get; set; }
+
+        // Llave foránea a Menu
+        public int ID_Menu { get; set; }
+        public MenuE Menu { get; set; }
+
+        // Llave foránea a AspNetUsers (Identidad)
+        public string ID_Usuario { get; set; }
+         
+        // Relación con IdentityUser (si está en el mismo contexto)
+        // public IdentityUserE Usuario { get; set; }
+    }
+}
